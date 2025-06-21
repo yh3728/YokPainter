a03d271fa81b88baff01207156082fd574df894d
yokomin12 <131192821+yh3728@users.noreply.github.com>
2025-06-21 23:11:13 +0300

    Delete build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/deploy_YokPainter2_1ac9b928b5.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/studying/cpp/YokPainter2/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/YokPainter2-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE D:/studying/cpp/YokPainter2/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/YokPainter2.exe
-    GENERATE_QT_CONF
-)
